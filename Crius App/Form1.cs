94e1f5213d13d4fcf5aa949eb06feda49a340110
AayushmanCrius <aayushmanandcrius@gmail.com>
2022-03-12 13:53:06 +0530

    Add project files.

---
+++ Crius App/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Crius_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(textBox1.Text,"Print",MessageBoxButtons.OK);
+        }
+
+        private void button_close_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are You Sure You Want To Quit", "Conform", MessageBoxButtons.YesNo , MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+    }
+}
